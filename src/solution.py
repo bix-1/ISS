3af04554e66ff097855a1ec27854eb316ed32b8d
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-12-17 21:35:23 +0100

    Added overlap-add & window function

--- src/solution.py
+++ src/solution.py
@@ -40,14 +40,44 @@ def dft(x):
                 for n in range(N)]
     return X
 
+def overlapAdd(x, h, N):
+    M = 1024
+    size = x[0].size
+    step = int(size / 2)
+    out = np.zeros(step * N + M, dtype=complex)
+
+    for i in range(0,N):
+        pos = step * i
+        ramec = np.zeros(M, dtype=complex)
+        ramec[:size] = x[i]
+        f = ss.lfilter(h, [1], ramec)
+        # f = np.fft.ifft(np.fft.fft(ramec, M) * h)
+        out[pos:pos+M] = out[pos:pos+M] + f
+
+    return out
+
+def split(x, N, size, step):
+    out = []
+    for i in range(0, N):
+        start = i * step
+        tmp = x[start : start + size]
+        out.append([])
+        out[i] = tmp.copy()
+
+    return out
+
+
 data_tone_off, fs = soundfile.read('../audio/maskoff_tone.wav')
-startOFF = 27200
+data_tone_off = data_tone_off[13200:63300]
+startOFF = 14400
 tone_off = data_tone_off[startOFF:startOFF+fs]
 t1 = np.arange(tone_off.size) / fs
 
 data_tone_on, fs = soundfile.read('../audio/maskon_tone.wav')
-startON = 28160
+data_tone_on = data_tone_on[11400:60600]
+startON = 5760
 tone_on = data_tone_on[startON:startON+fs]
+t2 = np.arange(tone_on.size) / fs
 
 # startOFF = 0
 # startON = 0
@@ -65,17 +95,6 @@ tone_on = data_tone_on[startON:startON+fs]
 # print(startOFF, startON)
 # quit()
 
-# fig, (ax1, ax2) = plt.subplots(2)
-# ax1.plot(np.arange(data_tone_off.size), data_tone_off)
-# ax2.plot(np.arange(data_tone_on.size), data_tone_on)
-# fig, (ax1, ax2) = plt.subplots(2)
-# ax1.plot(np.arange(data_tone_off[startOFF:startOFF+16000].size), data_tone_off[startOFF:startOFF+16000])
-# ax2.plot(np.arange(data_tone_on[startON:startON+16000].size), data_tone_on[startON:startON+16000])
-# plt.show()
-# quit()
-
-
-t2 = np.arange(tone_on.size) / fs
 
 t1 -= np.mean(t1)
 t2 -= np.mean(t2)
@@ -84,23 +103,16 @@ t1 /= np.abs(t1).max()
 t2 /= np.abs(t2).max()
 
 # ramce
+N = 99
 ramec_size_s = 0.02
 freq = 16000
-ramec_size = int(ramec_size_s * freq)
-step = int(0.01 * freq)
+ramec_size = int(ramec_size_s * fs)
+step = int(0.01 * fs)
 
-N = 99
-ramec_off = []
-ramec_on = []
+ramec_off = split(tone_off, N, ramec_size, step)
+ramec_on = split(tone_on, N, ramec_size, step)
 
-for i in range(0, N):
-    start = i * step
-    tmp1 = tone_off[start : start + ramec_size]
-    tmp2 = tone_on[start : start + ramec_size]
-    ramec_off.append([])
-    ramec_on.append([])
-    ramec_off[i] = tmp1.copy()
-    ramec_on[i] = tmp2.copy()
+
 
 
 prah = 20
@@ -110,57 +122,86 @@ base_freqsON = np.zeros(N)
 sgrOFF = np.zeros(shape=[N, samples], dtype=complex)
 sgrON = np.zeros(shape=[N, samples], dtype=complex)
 H = np.zeros(samples, dtype=complex)
-
-# resultB = autocorr(central_clip(ramec_on[5]))
-# L = np.argmax(np.abs(resultB[prah:])) + prah
-# base_freqsON[5] = fs / L
-# plt.figure(999, figsize=(6,3))
-# plt.plot(np.arange(resultB.size), resultB)
-# plt.show()
-# quit()
-
 for i in range(0, N):
     # mask off
     cc = central_clip(ramec_off[i])
     ac = autocorr(cc)
     L = lag(ac, prah)
     base_freqsOFF[i] = fs / L
+
+    wOFF = ramec_off[i] * np.hamming(ramec_size)
+
     # spec
-    sgrOFF[i] = np.fft.fft(ramec_off[i], samples)
+    # sgrOFF[i] = np.fft.fft(ramec_off[i], samples)
+    sgrOFF[i] = np.fft.fft(wOFF, samples)
 
     # mask on
     resultB = autocorr(central_clip(ramec_on[i]))
     L = np.argmax(np.abs(resultB[prah:])) + prah
     base_freqsON[i] = fs / L
+
+    wON = ramec_on[i] * np.hamming(ramec_size)
+
     # spec
-    sgrON[i] = np.fft.fft(ramec_on[i], samples)
+    sgrON[i] = np.fft.fft(wON, samples)
 
     # frequency characteristics
     H += sgrON[i] / sgrOFF[i]
 
+
 sgrOFF_log = np.transpose(10 * np.log10(np.abs(sgrOFF)**2))
 sgrON_log = np.transpose(10 * np.log10(np.abs(sgrON)**2))
+
 
 H = np.abs(H) / N
 io = np.fft.ifft(H)
 
-outT = ss.lfilter(H, 1.0, data_tone_off)
+outT = ss.lfilter(H, [1], data_tone_off)
 outT /= np.abs(outT).max()
 wav.write('tone.wav', fs, outT.astype(data_tone_off.dtype))
 
+step = int(ramec_size / 2)
+N = int(data_tone_off.size / step) - 1
+ramce = split(data_tone_off, N, ramec_size, step)
+outTov = overlapAdd(ramce, io, N)
+outTov /= np.abs(outTov).max()
+wav.write('ov_tone.wav', fs, outTov.astype(data_tone_off.dtype))
 
 sentenceOFF, fs = soundfile.read('../audio/maskoff_sentence.wav')
-outS = ss.lfilter(H, 1.0, sentenceOFF)
+step = int(ramec_size / 2)
+N = int(sentenceOFF.size / step) - 1
+ramce = split(sentenceOFF, N, ramec_size, step)
+outS = overlapAdd(ramce, io, N)
 outS /= np.abs(outS).max()
 wav.write('sentence.wav', fs, outS.astype(data_tone_off.dtype))
-
 
 # PROTOCOL DATA
 dir = '../img/'
 # --------------------------    DATA
 # fig, (ax1, ax2) = plt.subplots(2)
-# ax1.plot(t1, tone_off)
-# ax2.plot(t2, tone_on)
+# ax1.plot(np.arange(data_tone_off.size), data_tone_off)
+# ax2.plot(np.arange(data_tone_on.size), data_tone_on)
+
+
+# --------------------------    TONES
+fig, axs = plt.subplots(4)
+
+data_tone_off /= np.abs(data_tone_off).max()
+data_tone_on /= np.abs(data_tone_on).max()
+
+axs[0].plot(np.arange(data_tone_off.size), data_tone_off, )
+axs[1].plot(np.arange(data_tone_on.size), data_tone_on)
+axs[2].plot(np.arange(outT.size), outT)
+axs[3].plot(np.arange(outTov.size), outTov)
+
+axs[0].set_title('Mask off')
+axs[1].set_title('Mask on')
+axs[2].set_title('Sim')
+axs[3].set_title('Sim overlap-add')
+plt.tight_layout()
+plt.savefig(dir + 'tones.png')
+
+
 # --------------------------    [3] RAMCE
 x = np.arange(0., ramec_size_s, ramec_size_s / ramec_size)
 plt.figure(figsize=(10,4))
@@ -238,7 +279,7 @@ plt.savefig(dir + 'sgrs.png')
 plt.figure(figsize=(8,6))
 plt.suptitle("Frekvenčná charakteristika rúšky")
 
-plt.plot(np.arange(samples) / samples * fs/2, H)
+plt.plot(np.arange(H.size) / samples * fs/2, H)
 plt.xlabel('frekvencia')
 
 plt.tight_layout()
@@ -246,18 +287,24 @@ plt.savefig(dir + 'freqch.png')
 # --------------------------    [7] IMPULZNA ODOZVA
 plt.figure(figsize=(8,6))
 plt.suptitle("Impulzná odozva rúšky")
-plt.plot(np.arange(samples), np.abs(io))
+plt.plot(np.arange(io.size), io.real)
 plt.xlabel('čas')
 
 plt.tight_layout()
 plt.savefig(dir + 'impOdoz.png')
 # --------------------------    [8] VYSLEDOK
-fig, axs = plt.subplots(4)
+# plt.close('all')
+fig, axs = plt.subplots(5)
 fig.set_size_inches(10, 10)
 
 sentenceOFF, fs = soundfile.read('../audio/maskoff_sentence.wav')
 sentenceON, fs = soundfile.read('../audio/maskon_sentence.wav')
-sim = ss.lfilter(H, 1.0, sentenceOFF)
+
+step = int(ramec_size / 2)
+N = int(sentenceOFF.size / step) - 1
+ramce = split(sentenceOFF, N, ramec_size, step)
+sim = overlapAdd(ramce, io, N)
+# sim = ss.lfilter(H, 1.0, sentenceOFF)
 sim /= np.abs(sim).max()
 
 sentenceOFF /= np.abs(sentenceOFF).max()
@@ -266,21 +313,22 @@ sentenceON /= np.abs(sentenceON).max()
 
 axs[0].set_title('Veta bez rúšky')
 axs[0].plot(np.arange(sentenceOFF.size), sentenceOFF, label='off')
-axs[0].plot(np.arange(sentenceON.size), sentenceON, label='on')
 axs[0].legend()
 
-# axs[1].set_title('Veta s rúškou')
-# axs[1].plot(np.arange(sentenceON.size), sentenceON)
+axs[1].set_title('Veta s rúškou')
+axs[1].plot(np.arange(sentenceON.size), sentenceON)
 
 axs[2].set_title('Veta so simulovanou rúškou')
-axs[2].plot(np.arange(sentenceOFF.size), sentenceOFF, label='off')
-axs[2].plot(np.arange(sim.size), sim, label='sim')
+axs[2].plot(np.arange(sim.size), sim)
 
-axs[3].plot(np.arange(sim.size), sim, label='sim')
 axs[3].plot(np.arange(sentenceOFF.size), sentenceOFF, label='off')
+axs[3].plot(np.arange(sim.size), sim, label='sim')
 
-axs[2].legend()
+axs[4].plot(np.arange(sim.size), sim, label='sim')
+axs[4].plot(np.arange(sentenceOFF.size), sentenceOFF, label='off')
+
 axs[3].legend()
+axs[4].legend()
 
 plt.tight_layout()
 plt.savefig(dir + 'outcome.png')
